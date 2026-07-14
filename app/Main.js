e6a74cdccd58f0ebbf5bf4cd98a9b29e1b1cf34a
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-07-14 14:19:59 -0400

    Add files via upload

--- app/Main.js
+++ app/Main.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
+import Overlay from "./components/Overlay";
 
 function MainPage() {
   return (
     <>
       <Header />
+      <Overlay />
       <p>This is some test content.</p>
     </>
   );
--- app/components/Header.js
+++ app/components/Header.js
@@ -2,26 +2,48 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
-      <p>
-        <em>You're not alone.</em>
-      </p>
-      <label for="nav-select">Find help with </label>
-      <select id="nav-select">
-        <option value="risk">knowing your risk</option>
-        <option value="clarity">gaining clarity</option>
-        <option value="housing">safe housing</option>
-        <option value="food">food</option>
-        <option value="utilities">utilities</option>
-        <option value="transportation">transportation</option>
-        <option value="health">health and healing</option>
-        <option value="immigration">imigration</option>
-        <option value="addiction">substance use</option>
-        <option value="legal">legal protection</option>
-        <option value="kids">protecting your kids</option>
-        <option></option>
-      </select>
-    </header>
+    <>
+      <header>
+        <div id="header-content">
+          <svg viewBox="0 0 500 400" id="logo">
+            <defs>
+              <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                <stop offset="0%" stop-color="#82027e" />
+                <stop offset="50%" stop-color="#1b007d" />
+                <stop offset="100%" stop-color="#024b7c" />
+              </linearGradient>
+            </defs>
+            <text x="40" y="120">
+              YOU'RE
+            </text>
+            <text x="40" y="220" id="not-word">
+              NOT
+            </text>
+            <text x="40" y="320">
+              ALONE
+            </text>
+          </svg>
+          <div id="nav-select">
+            <label for="nav-select">Find help with </label>
+            <select id="nav-select">
+              <option value="clarity">gaining clarity</option>
+              <option value="risk">knowing your risk</option>
+              <option value="housing">safe housing</option>
+              <option value="food">food</option>
+              <option value="utilities">utilities</option>
+              <option value="transportation">transportation</option>
+              <option value="health">health and healing</option>
+              <option value="immigration">imigration</option>
+              <option value="addiction">substance use</option>
+              <option value="legal">legal protection</option>
+              <option value="kids">protecting your kids</option>
+              <option></option>
+            </select>
+          </div>
+        </div>
+      </header>
+      <div id="header-transition"></div>
+    </>
   );
 }
 
