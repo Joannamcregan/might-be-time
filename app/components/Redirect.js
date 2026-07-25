b9ca4a3e7adbfdb8f0557a71a3afd428d70f9cc2
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-07-25 09:15:28 -0400

    Add files via upload

--- app/components/Redirect.js
+++ app/components/Redirect.js
@@ -1,10 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
+function redirectPage(destination = "https://cnn.com") {
+  if (localStorage.getItem("selectedStyle")) {
+    destination = localStorage.getItem("selectedStyle");
+  }
+  if (destination == "nbc") {
+    destination = "https://nbcnews.com";
+  } else if (destination == "fox") {
+    destination = "https://foxnews.com";
+  } else if (destination == "aljazeera") {
+    destination = "https://aljazeera.com";
+  } else if (destination == "cnn") {
+    destination = "https://cnn.com";
+  }
+  window.location.replace(destination);
+}
 
 function QuickExit(props) {
   return (
-    <>
-      <p id="exit">Exit</p>
-    </>
+    <p
+      id="exit"
+      onClick={props => {
+        redirectPage(props.redirectSelection);
+      }}
+    >
+      Exit
+    </p>
   );
 }
 
