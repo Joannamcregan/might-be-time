f9050cc9318d648a5663177052d132197c4c02ea
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-09-21 07:17:01 -0400

    Add files via upload

--- app/components/MotionRedirect.js
+++ app/components/MotionRedirect.js
@@ -3,7 +3,7 @@ import { redirect } from "react-router-dom";
 import DispatchContext from "../DispatchContext";
 import StateContext from "../StateContext";
 
-function VisibilityRedirect(props) {
+function MotionRedirect(props) {
   const appState = useContext(StateContext);
   const appDispatch = useContext(DispatchContext);
 
@@ -13,15 +13,18 @@ function VisibilityRedirect(props) {
 
   useEffect(() => {
     let appDestination = appState.redirectSelection;
-    document.addEventListener("visibilitychange", appDestination => {
-      if (document.visibilityState === "hidden"){
+    document.addEventListener("devicemotion", appDestination => {
+    let motion = (event) => `${event.acceleration.x} m/s2 on x-axis, ${event.acceleration.y} m/s2 on y-axis, ${event.acceleration.z} m/s on z-axis`;
+      if (event.acceleration.x > 15 ||
+        event.acceleration.y > 15 ||
+        event.acceleration.z > 15){
         redirect(appDestination);
         localStorage.removeItem("selectedStyle");
       }
     });
     return () => {
       document.removeEventListener(
-        "visibilitychange",
+        "devicemotion",
         redirect(redirectSelection)
       );
     };
@@ -29,4 +32,4 @@ function VisibilityRedirect(props) {
   return <></>;
 }
 
-export default VisibilityRedirect;
+export default MotionRedirect;
